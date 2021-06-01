2d109baeea4c3d561aa70d0bae2e3a40d1425366
Alexander Korenchikov <alexander.korenchikov@devexpress.com>
2021-06-01 20:12:39 +0300

    another sample

--- Program.cs
+++ Program.cs
@@ -17,8 +17,8 @@ namespace CollectSettingsBeforeRender
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddDevExpressBlazor();
             await builder.Build().RunAsync();
         }
     }
